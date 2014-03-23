cc8101baa9667ca62e28ab2c33135ce2be40819e
Craig <vxdhost@gmail.com>
2014-03-24 11:34:50 +1300

    Encryption class

---
+++ system/library/encryption.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * openDeal - Opensource Deals Platform
+ *
+ * @author      Craig Smith <vxdhost@gmail.com>
+ * @copyright   2014 Craig Smith
+ * @link        https://github.com/openDeal/openDeal
+ * @license     https://raw.githubusercontent.com/openDeal/openDeal/master/LICENSE
+ * @since       1.0.0
+ * @package     Core
+ * GPLV3 Licence
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+final class Encryption {
+
+    private $key;
+    private $iv;
+
+    public function __construct($key) {
+        $this->key = hash('sha256', $key, true);
+        $this->iv = mcrypt_create_iv(32, MCRYPT_RAND);
+    }
+
+    public function encrypt($value) {
+        return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->key, $value, MCRYPT_MODE_ECB, $this->iv)), '+/=', '-_,');
+    }
+
+    public function decrypt($value) {
+        return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->key, base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB, $this->iv));
+    }
+
+}
+
+?>
