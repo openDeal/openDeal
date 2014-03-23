b741035184eb95f74f405004782e2bbafc9ae78f
Craig <vxdhost@gmail.com>
2014-03-24 11:35:24 +1300

    Language class

---
+++ system/library/language.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * openDeal - Opensource Deals Platform
+ *
+ * @author      Craig Smith <vxdhost@gmail.com>
+ * @copyright   2014 Craig Smith
+ * @link        https://github.com/openDeal/openDeal
+ * @license     https://raw.githubusercontent.com/openDeal/openDeal/master/LICENSE
+ * @since       1.0.0
+ * @package     Core
+ * GPLV3 Licence
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+class Language {
+	private $default = 'english';
+	private $directory;
+	private $data = array();
+
+	public function __construct($directory) {
+		$this->directory = $directory;
+	}
+
+	public function get($key) {
+		return (isset($this->data[$key]) ? $this->data[$key] : $key);
+	}
+
+	public function load($filename) {
+		$file = DIR_LANGUAGE . $this->directory . '/' . $filename . '.php';
+
+		if (file_exists($file)) {
+			$_ = array();
+
+			require($file);
+
+			$this->data = array_merge($this->data, $_);
+
+			return $this->data;
+		}
+
+		$file = DIR_LANGUAGE . $this->default . '/' . $filename . '.php';
+
+		if (file_exists($file)) {
+			$_ = array();
+
+			require($file);
+
+			$this->data = array_merge($this->data, $_);
+
+			return $this->data;
+		} else {
+                    
+                    //Actually if no language file, to bad, they can fix it themeselves later!!!!
+		//	trigger_error('Error: Could not load language ' . $filename . '!');
+		//	exit();
+		}
+	}
+}
+?>
