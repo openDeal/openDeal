f862633cdde016c9d10e5c1405289d8901be2881
Craig <vxdhost@gmail.com>
2014-05-30 16:20:15 +1200

    Coupon

--- public/language/english/account/order.php
+++ public/language/english/account/order.php
@@ -1,43 +1,37 @@
 <?php
-// Heading 
-$_['heading_title']         = 'Order History';
-
-// Text
-$_['text_account']          = 'Account';
-$_['text_order']            = 'Order Information';
-$_['text_order_detail']     = 'Order Details';
-$_['text_invoice_no']       = 'Invoice No.:';
-$_['text_order_id']         = 'Order ID:';
-$_['text_status']           = 'Status:';
-$_['text_date_added']       = 'Date Added:';
-$_['text_customer']         = 'Customer:';
-$_['text_shipping_address'] = 'Shipping Address';
-$_['text_shipping_method']  = 'Shipping Method:';
-$_['text_payment_address']  = 'Payment Address';
-$_['text_payment_method']   = 'Payment Method:';
-$_['text_products']         = 'Products:';
-$_['text_total']            = 'Total:';
-$_['text_comment']          = 'Order Comments';
-$_['text_history']          = 'Order History';
-$_['text_success']          = 'You have successfully added the products from order ID #%s to your cart!';
-$_['text_empty']            = 'You have not made any previous orders!';
-$_['text_error']            = 'The order you requested could not be found!';
-
-// Column
-$_['column_name']           = 'Product Name';
-$_['column_model']          = 'Model';
-$_['column_quantity']       = 'Quantity';
-$_['column_price']          = 'Price';
-$_['column_total']          = 'Total';
-$_['column_action']         = 'Action';
-$_['column_date_added']     = 'Date Added';
-$_['column_status']         = 'Status';
-$_['column_comment']        = 'Comment';
-$_['column_delivery']       = 'Delivery';
-
-$_['text_collect'] = 'Collect';
+$_['button_continue'] = 'My Account';
 $_['button_coupon'] = 'View Coupon';
 $_['button_view'] = 'View Order';
+$_['column_action'] = 'Action';
+$_['column_comment'] = 'Comment';
+$_['column_date_added'] = 'Date Added';
+$_['column_delivery'] = 'Delivery';
+$_['column_model'] = 'Model';
+$_['column_name'] = 'Product Name';
+$_['column_price'] = 'Price';
+$_['column_quantity'] = 'Quantity';
+$_['column_status'] = 'Status';
+$_['column_total'] = 'Total';
+$_['heading_title'] = 'Order History';
+$_['text_account'] = 'Account';
+$_['text_collect'] = 'Collect';
+$_['text_comment'] = 'Order Comments';
+$_['text_customer'] = 'Customer:';
+$_['text_date_added'] = 'Date Added:';
+$_['text_empty'] = 'Sorry - you haven\'t made any previous orders, yet...';
+$_['text_error'] = 'Sorry - the order you requested could not be found. Please check the details and try again.';
+$_['text_history'] = 'Order History';
+$_['text_invoice_no'] = 'Invoice Number:';
+$_['text_order'] = 'Order Information';
+$_['text_order_detail'] = 'Order Details';
+$_['text_order_id'] = 'Order ID:';
+$_['text_payment_address'] = 'Payment Address';
+$_['text_payment_method'] = 'Payment Method:';
+$_['text_products'] = 'Products:';
+$_['text_shipping_address'] = 'Shipping Address';
+$_['text_shipping_method'] = 'Shipping Method:';
+$_['text_status'] = 'Status:';
+$_['text_total'] = 'Total:';
 
 
 $_['text_security_code'] = 'Coupon Security Code';
