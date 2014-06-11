e933fee11f3c9dab4ee98c8ac80457a55b6a73d2
Craig <vxdhost@gmail.com>
2014-06-12 10:58:30 +1200

    Url Incorrect on dashboard

--- public/controller/module/facebook.php
+++ public/controller/module/facebook.php
@@ -1,57 +1,61 @@
-<?php  
+<?php
+
 class ControllerModuleFacebook extends \Core\Controller {
-	
-	protected function index($setting) {
-		static $module = 0;
-		
-		$this->document->addScript('public/view/javascript/facebook/facebook_comment.js');
-		
-		$this->data['adminuid'] = $this->config->get('adminuid');
-		
-		$this->data['appid'] = $this->config->get('appid');
-                
-                if($this->config->get('adminuid')){
-                $this->document->addMeta('fb:admins', '<meta property="fb:admins" content="' . $this->config->get('adminuid') .'"/>');
-                }
-                if($this->config->get('appid')){
-                $this->document->addMeta('fb:appid', '<meta property="fb:appid" content="' . $this->config->get('appid') .'"/>');
-                }
-                /*
-                 * $this->data['adminuid'] = $this->config->get('adminuid');
-		
-		$this->data['appid'] = $this->config->get('appid');
-                 */
-                
-		
-		$this->data['width'] = $setting['width'];
-		
-		$this->data['numpost'] = $setting['numpost'];
-		
-		
-		$siteurl = "";
-		
-		if($setting['store'] != "default" && $setting['store']){
-			$siteurl = $setting['store'];
-		} else {
-			$siteurl = $this->config->get('config_url');
-		}
-		
-		if($setting['urltype']=="1"){
-			$this->data['siteurl'] = $siteurl;
-		} else if($setting['urltype']=="2") {
-			$this->data['siteurl'] = $_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"];
-		} else {
-			$this->data['siteurl'] = $siteurl;
-		}
-		
-		$this->data['colorscheme'] = $setting['colorscheme'];
-			
-		$this->data['module'] = $module++;
-		
-			$this->template = 'module/facebook.phtml';
-		
-		
-		$this->render();
-	}
+
+    protected function index($setting) {
+        static $module = 0;
+
+        $this->document->addScript('public/view/javascript/facebook/facebook_comment.js');
+
+        $this->data['adminuid'] = $this->config->get('adminuid');
+
+        $this->data['appid'] = $this->config->get('appid');
+
+        if ($this->config->get('adminuid')) {
+            $this->document->addMeta('fb:admins', '<meta property="fb:admins" content="' . $this->config->get('adminuid') . '"/>');
+        }
+        if ($this->config->get('appid')) {
+            $this->document->addMeta('fb:appid', '<meta property="fb:appid" content="' . $this->config->get('appid') . '"/>');
+        }
+        /*
+         * $this->data['adminuid'] = $this->config->get('adminuid');
+
+          $this->data['appid'] = $this->config->get('appid');
+         */
+
+
+        $this->data['width'] = $setting['width'];
+
+        $this->data['numpost'] = $setting['numpost'];
+
+
+        $siteurl = "http://" . $this->request->server["SERVER_NAME"];
+
+      /*  if ($setting['store'] != "default" && $setting['store']) {
+            $siteurl = $setting['store'];
+        } else {
+            $siteurl = $this->config->get('config_url');
+        }*/
+
+
+        if ((isset($this->request->get['deal_id']) || isset($this->request->get['freepon_id'])) && $setting['urltype'] == "2") {
+            $this->data['siteurl'] = $siteurl . $_SERVER["REQUEST_URI"];
+        } else {
+            $this->data['siteurl'] = $siteurl;
+        }
+
+
+
+        $this->data['colorscheme'] = $setting['colorscheme'];
+
+        $this->data['module'] = $module++;
+
+        $this->template = 'module/facebook.phtml';
+
+
+        $this->render();
+    }
+
 }
+
 ?>
