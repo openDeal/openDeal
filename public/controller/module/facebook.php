76e153ac0fd665a78f61e6f2d33a24a215bfa854
Craig <vxdhost@gmail.com>
2014-06-12 11:49:47 +1200

    facebook

--- public/controller/module/facebook.php
+++ public/controller/module/facebook.php
@@ -15,7 +15,7 @@ class ControllerModuleFacebook extends \Core\Controller {
             $this->document->addMeta('fb:admins', '<meta property="fb:admins" content="' . $this->config->get('adminuid') . '"/>');
         }
         if ($this->config->get('appid')) {
-            $this->document->addMeta('fb:appid', '<meta property="fb:appid" content="' . $this->config->get('appid') . '"/>');
+            $this->document->addMeta('fb:app_id', '<meta property="fb:appid" content="' . $this->config->get('appid') . '"/>');
         }
         /*
          * $this->data['adminuid'] = $this->config->get('adminuid');
